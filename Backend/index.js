4b5200395250a622af0653eac95120cacf38407d
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-08-10 01:41:49 +0530

    backend basic setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,14 @@
-console.log("Hello from the backend!");
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
