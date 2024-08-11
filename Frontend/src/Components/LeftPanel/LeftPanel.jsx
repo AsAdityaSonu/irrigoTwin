1a5b255664cb4b6045f38275b74043cd9b5933d9
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-08-11 18:45:23 +0530

    leftPanel updated

--- Frontend/src/Components/LeftPanel/LeftPanel.jsx
+++ Frontend/src/Components/LeftPanel/LeftPanel.jsx
@@ -26,7 +26,9 @@ function LeftPanel() {
                 to="/"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
@@ -48,7 +50,9 @@ function LeftPanel() {
                 to="/dashboard"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
@@ -63,7 +67,7 @@ function LeftPanel() {
                   <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z" />
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Dashboard</span>
-                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium bg-gray-300 rounded-full">
+                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium bg-gray-200 rounded-full">
                   Live
                 </span>
               </NavLink>
@@ -73,7 +77,9 @@ function LeftPanel() {
                 to="/about"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
@@ -94,7 +100,9 @@ function LeftPanel() {
                 to="/users"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
@@ -108,7 +116,7 @@ function LeftPanel() {
                   <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
                 </svg>
                 <span className="flex-1 ms-3 whitespace-nowrap">Users</span>
-                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium bg-gray-300 rounded-full">
+                <span className="inline-flex items-center justify-center px-2 ms-3 text-sm font-medium bg-gray-200 rounded-full">
                   Pro
                 </span>
               </NavLink>
@@ -118,7 +126,9 @@ function LeftPanel() {
                 to="signin"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
@@ -145,7 +155,9 @@ function LeftPanel() {
                 to="signup"
                 className={({ isActive }) =>
                   `flex items-center p-2 rounded-lg  ${
-                    isActive ? "bg-[#9afcd9] text-grapy-500" : "text-gray-900 hover:bg-gray-100 group"
+                    isActive
+                      ? "bg-[#9afcd9] text-grapy-500"
+                      : "text-gray-900 hover:bg-gray-100 group"
                   }`
                 }
               >
