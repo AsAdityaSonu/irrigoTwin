ff5704d92c36af4ba3caa439f686bbc8891a62b4
Aditya Pandey <105072228+AsAdityaSonu@users.noreply.github.com>
2024-08-13 01:52:05 +0530

    database connection done

--- Backend/index.js
+++ Backend/index.js
@@ -1,13 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './src/db/Database.js';
 
+// ------------------ Setup ------------------
 dotenv.config();
 const app = express();
+connectDB();
 
+// ------------------ Route ------------------
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
+app.get('/favicon.ico', (req, res)=>{
+  res.status(204);
+})
+
+// ------------------ Server ------------------
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
